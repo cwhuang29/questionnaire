54fd92a5babb5911e508dd93e72258539b27c080
靖宜 陳 <b10709032@gapps.ntust.edu.tw>
2021-11-21 13:20:25 +0800

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>header</p>
+    </header>
+  </div>
+);
+
+export default App;
