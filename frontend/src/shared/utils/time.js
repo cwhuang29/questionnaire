7d807d3b77dc65d638b7519ff5ab648c6ce245da
cwhuang29 <hq678835@gmail.com>
2022-04-26 00:41:38 +0800

    Update frontend

--- frontend/src/shared/utils/time.js
+++ frontend/src/shared/utils/time.js
@@ -3,5 +3,7 @@ export const getTodayDate = () => {
   return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
 };
 
+// Backend stores time in the UTC time zone. When the API responses with data such as { "createdAt":"2022-04-25T15:33:13.056Z" } (type is time.Time)
+// The JS new Date(value) changes time zone from UTC to user's location
 export const getDisplayTime = (date = new Date()) =>
   `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${`0${date.getMinutes()}`.slice(-2)}`;
